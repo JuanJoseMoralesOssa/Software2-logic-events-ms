b6d571fbda691f1a12f9eab87540b183f64a9b39
juan <juanjomoss@gmail.com>
2024-11-24 19:12:25 -0500

    .envexample

--- src/config/notificaciones.config.ts
+++ src/config/notificaciones.config.ts
@@ -1,18 +1,18 @@
 export namespace NotificacionesConfig {
   export const subject2fa: string = 'Codigo de verificacion';
   export const subjectClientRequest: string = 'Codigo de cliente';
-  export const urlNotifiAdvisorDecisionEmail: string =
-    'https://localhost:5258/Notification/advisor-decision-mail';
-  export const urlNotificationClientRequestEmail: string =
-    'https://localhost:5258/Notification/client-request-mail';
-  export const urlNotifiPasswordRecoveryEmail: string =
-    'https://localhost:5258/Notification/password-recovery-mail';
-  export const urlNotifications2fa: string =
-    'https://localhost:5258/Notification/send-mail-2fa';
-  export const urlNotificationsSms: string =
-    'https://localhost:5258/Notification/send-sms';
-  export const urlNotificationsCredentialsMail: string =
-    'https://localhost:5258/Notification/credentials-mail';
+  // export const urlNotifiAdvisorDecisionEmail: string =
+  //   'https://localhost:5258/Notification/advisor-decision-mail';
+  // export const urlNotificationClientRequestEmail: string =
+  //   'https://localhost:5258/Notification/client-request-mail';
+  // export const urlNotifiPasswordRecoveryEmail: string =
+  //   'https://localhost:5258/Notification/password-recovery-mail';
+  // export const urlNotifications2fa: string =
+  //   'https://localhost:5258/Notification/send-mail-2fa';
+  // export const urlNotificationsSms: string =
+  //   'https://localhost:5258/Notification/send-sms';
+  // export const urlNotificationsCredentialsMail: string =
+  //   'https://localhost:5258/Notification/credentials-mail';
   export const urlNotificationBienvenida: string =
     'http://localhost:5258/Notificaciones/correo_bienvenida';
 
